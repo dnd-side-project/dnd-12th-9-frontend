50d4fb88738639dc98f1c8f402d3bae9f3322d32
keemsebeen <sebin0580@gmail.com>
2025-01-23 18:55:47 +0900

    chore : eslint 적용

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,9 @@
+import { Button } from '@repo/ui/components/Button';
+
 export default function Home() {
   return (
     <div>
+      <Button>dd</Button>
       <footer className="text-blue-500">footer</footer>
     </div>
   );
