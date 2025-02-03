a6c72e4be3d2d2b8ded50b2bd9cfef5fc3dd1037
keemsebeen <sebin0580@gmail.com>
2025-02-04 01:36:09 +0900

    feat: text storybook 구현

---
+++ apps/storybook/stories/Text.stories.tsx
@@ -0,0 +1,39 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Text } from '@repo/ui/Text';
+
+const meta = {
+  title: 'components/Text',
+  component: Text,
+  tags: ['autodocs'],
+} satisfies Meta<typeof Text>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Basic: Story = {
+  args: {
+    type: 'Heading1',
+    weight: 'semibold',
+    children: 'Sbooky',
+  },
+  argTypes: {
+    type: {
+      options: [
+        'Heading1',
+        'Heading2',
+        'Heading3',
+        'Title1',
+        'Title2',
+        'Body1',
+        'Body2',
+        'caption',
+      ],
+      control: { type: 'select' },
+    },
+    weight: {
+      options: ['regular', 'medium', 'semibold', 'bold'],
+      control: { type: 'select' },
+    },
+  },
+  render: (args) => <Text {...args} />,
+};
