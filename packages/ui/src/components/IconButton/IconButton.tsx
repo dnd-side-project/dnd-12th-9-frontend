65c62995e391957e4a5c71374dd9dd70425ad759
DongjaJ <jdh981118@naver.com>
2025-02-04 02:37:30 +0900

    feat: icon-button에 restProps 적용

--- packages/ui/src/components/IconButton/IconButton.tsx
+++ packages/ui/src/components/IconButton/IconButton.tsx
@@ -4,10 +4,15 @@ import { cn } from '../../lib/core';
 
 export type IconButtonProps = ComponentProps<'button'>;
 
-export const IconButton = ({ children, className }: PropsWithChildren<IconButtonProps>) => {
+export const IconButton = ({
+  children,
+  className,
+  ...restProps
+}: PropsWithChildren<IconButtonProps>) => {
   return (
     <button
       className={cn('flex h-full flex-shrink-0 items-center justify-center px-4 py-3', className)}
+      {...restProps}
     >
       {children}
     </button>
