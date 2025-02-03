e41b55b23b2226f5df319311be151aea6f10120e
DongjaJ <jdh981118@naver.com>
2025-02-04 02:01:47 +0900

    feat: icon-button 컴포넌트 추가

---
+++ packages/ui/src/components/IconButton/IconButton.tsx
@@ -0,0 +1,15 @@
+import { ComponentProps, PropsWithChildren } from 'react';
+
+import { cn } from '../../lib/core';
+
+export type IconButtonProps = ComponentProps<'button'>;
+
+export const IconButton = ({ children, className }: PropsWithChildren<IconButtonProps>) => {
+  return (
+    <button
+      className={cn('flex h-full flex-shrink-0 items-center justify-center px-4 py-3', className)}
+    >
+      {children}
+    </button>
+  );
+};
