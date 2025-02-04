6d4abaf612eb2365e53b780ea04a8b844e3d1767
keemsebeen <sebin0580@gmail.com>
2025-02-04 19:06:31 +0900

    feat: framer-motion을 활용하여 modal 컴포넌트 구현

---
+++ packages/ui/src/components/Modal/Modal.tsx
@@ -0,0 +1,51 @@
+import { motion } from 'framer-motion';
+
+import { FADE_IN_ANIMATION } from '@/lib/motions';
+
+import { Portal } from '../Portal';
+
+import { Content } from './ModalContent';
+
+type Props = {
+  /**
+   * Indicates whether the modal is currently open.
+   * @default false
+   */
+  isOpen: boolean;
+  /**
+   * optional, can be used to handle modal closing or other actions.
+   */
+  onClickOutside?: VoidFunction;
+  /**
+   * optional, can be used to display a graphic in the modal.
+   * @default false
+   */
+  graphic?: boolean;
+  /**
+   * The content to be displayed inside the modal.
+   */
+  children: React.ReactNode;
+};
+
+export function Modal({ isOpen, onClickOutside, graphic = false, children }: Props) {
+  const onClickOutsideDefault = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (e.target instanceof HTMLElement && e.target === e.currentTarget && onClickOutside) {
+      onClickOutside();
+    }
+  };
+
+  return (
+    <Portal isOpen={isOpen} mode="wait">
+      <motion.div
+        onClick={onClickOutsideDefault}
+        variants={FADE_IN_ANIMATION}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        className="fixed inset-0 z-50 flex items-center justify-center bg-gray-800 bg-opacity-50"
+      >
+        <Content graphic={graphic}>{children}</Content>
+      </motion.div>
+    </Portal>
+  );
+}
--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -1,2 +1,4 @@
 export { Button } from './Button';
 export { Text } from './Text';
+export { Portal } from './Portal';
+export { Modal } from './Modal';
