5fa9955ec8c367683d9af05f6174b3c1f0ec5b0a
DongjaJ <jdh981118@naver.com>
2025-02-04 20:35:44 +0900

    fix: polymorphicComponentRef 제거

--- packages/ui/src/polymorphics.ts
+++ packages/ui/src/polymorphics.ts
@@ -6,7 +6,5 @@ export type AsProp<C extends ElementType> = {
 
 export type KeyWithAs<C extends ElementType, Props> = keyof (AsProp<C> & Props);
 
-export type PolymorphicRef<C extends ElementType> = ComponentProps<C>['ref'];
-
 export type PolymorphicComponentProps<C extends ElementType, Props = object> = (Props & AsProp<C>) &
   Omit<ComponentProps<C>, KeyWithAs<C, Props>>;
