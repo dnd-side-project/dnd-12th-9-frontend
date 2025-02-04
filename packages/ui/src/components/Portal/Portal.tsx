dc9d7f7a450d28c15ea8ea27db880c6c5d7583f9
keemsebeen <sebin0580@gmail.com>
2025-02-04 19:05:21 +0900

    feat: animatepresence 활용하여 portal 컴포넌트 구현

---
+++ packages/ui/src/components/Portal/Portal.tsx
@@ -0,0 +1,37 @@
+import { PropsWithChildren, useEffect, useState } from 'react';
+
+import { AnimatePresence } from 'framer-motion';
+import { createPortal } from 'react-dom';
+
+export function PortalWrapper({ children }: PropsWithChildren) {
+  const [container, setContainer] = useState<Element | null>(null);
+
+  useEffect(() => {
+    setContainer(document.body);
+  }, []);
+
+  if (!container) return null;
+
+  return createPortal(<>{children}</>, container);
+}
+
+type Props = {
+  /**
+   * Indicates whether the modal is currently open.
+   * @default false
+   */
+  isOpen: boolean;
+  /**
+   * The content to render within the portal.
+   *  @default 'wait'
+   */
+  mode?: 'wait' | 'sync' | 'popLayout';
+} & PropsWithChildren;
+
+export function Portal({ isOpen, mode = 'wait', children }: PropsWithChildren<Props>) {
+  return (
+    <PortalWrapper>
+      <AnimatePresence mode={mode}>{isOpen && children}</AnimatePresence>
+    </PortalWrapper>
+  );
+}
