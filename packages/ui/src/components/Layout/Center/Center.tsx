d883be3b52bfc68a7787270ae1f10fd566b1a6b2
DongjaJ <jdh981118@naver.com>
2025-02-09 17:41:33 +0900

    refactor: center 컴포넌트의 as prop을 restProps로 받도록  변경

--- packages/ui/src/components/Layout/Center/Center.tsx
+++ packages/ui/src/components/Layout/Center/Center.tsx
@@ -6,13 +6,10 @@ import { type BoxProps, Box } from '../Box';
 export const Center: <C extends ElementType = 'div'>(
   props: PropsWithChildren<BoxProps<C>>
 ) => JSX.Element = <C extends ElementType = 'div'>({
-  as,
   className,
   ...rest
 }: PropsWithChildren<BoxProps<C>>) => {
   const typesRest = rest as BoxProps<C>;
 
-  return (
-    <Box className={cn('flex items-center justify-center', className)} as={as} {...typesRest} />
-  );
+  return <Box className={cn('flex items-center justify-center', className)} {...typesRest} />;
 };
