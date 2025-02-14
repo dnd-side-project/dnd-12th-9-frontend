411cac34cb7524ffaa48c88e993ff96d2e1069f8
DongjaJ <jdh981118@naver.com>
2025-02-14 13:51:33 +0900

    feat: 빈 책장 ui 스타일링 수정

--- apps/web/app/books/page.tsx
+++ apps/web/app/books/page.tsx
@@ -7,7 +7,14 @@ import { useState } from 'react';
 import { Button } from '@repo/ui/components/Button';
 import { Chip } from '@repo/ui/components/Chip';
 import { Header } from '@repo/ui/components/Header';
-import { CenterStack, HStack, PageLayout, Spacing, Stack } from '@repo/ui/components/Layout';
+import {
+  Center,
+  CenterStack,
+  HStack,
+  PageLayout,
+  Spacing,
+  Stack,
+} from '@repo/ui/components/Layout';
 import { Text } from '@repo/ui/components/Text';
 
 import EMPTY_STATE from '../../../../public/assets/EMPTY_STATE.png';
@@ -48,15 +55,20 @@ const BookListPage = () => {
         <Spacing className="h-20" />
         <CenterStack className="grow">
           <Image src={EMPTY_STATE} alt="비어있는 책장" width={120} height={120} />
+          <Spacing className="h-4" />
           <Text className="text-gray-900" weight="semibold" type="Title1">
             책장이 비어 있어요...
           </Text>
+          <Spacing className="h-2" />
           <Text className="text-gray-500" weight="medium" type="Body2">
             책을 검색하고 내 책장에 추가해보세요.
           </Text>
-          <Button variant="primary50" size="sm">
-            등록하기
-          </Button>
+          <Spacing className="h-5" />
+          <Center className="py-1">
+            <Button variant="primary50" size="sm">
+              등록하기
+            </Button>
+          </Center>
         </CenterStack>
       </Stack>
     </PageLayout>
