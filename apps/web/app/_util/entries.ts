2e9b7f04b93ceccd60d9b315b9398f75649ddea2
DongjaJ <jdh981118@naver.com>
2025-02-15 02:58:30 +0900

    fix: entries 네이밍 변경

--- apps/web/app/_util/entries.ts
+++ apps/web/app/_util/entries.ts
@@ -2,6 +2,6 @@ type Entries<T> = {
   [K in keyof T]: [K, T[K]];
 }[keyof T][];
 
-export const Entries = <T extends object>(obj: T) => {
+export const entries = <T extends object>(obj: T) => {
   return Object.entries(obj) as Entries<T>;
 };
