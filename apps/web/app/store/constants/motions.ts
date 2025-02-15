d36f8c647f7dab7bfb9688c34f0eb90727643855
keemsebeen <sebin0580@gmail.com>
2025-02-15 22:59:42 +0900

    feat: 스토어페이지 motions 상수 분리

---
+++ apps/web/app/store/constants/motions.ts
@@ -0,0 +1,9 @@
+export const SLIDER_ANIMATION = {
+  initial: { x: '50%' },
+  animate: { x: ['100%', '-100%'] },
+  transition: {
+    repeat: Infinity,
+    duration: 10,
+    ease: 'linear',
+  },
+};
