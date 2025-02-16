4f32d5b792b6e660ec09eebe7f0a537aafbd7da9
DongjaJ <jdh981118@naver.com>
2025-02-17 01:21:02 +0900

    feat: accessToken이 쿠키에 없을 시 로그인 페이지로 리다이렉트하는 코드 추가(개발 편의성을 위해 주석 처리)

---
+++ apps/web/middleware.ts
@@ -0,0 +1,20 @@
+// FIXME 액세스 토큰이 없을때 로그인 페이지로 리다이렉트 시키는 코드, 개발 편의성을 위해 주석처리
+
+// import { cookies } from 'next/headers';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function middleware(_request: NextRequest) {
+  // const cookieStore = await cookies();
+  // const accessToken = cookieStore.get('accessToken');
+
+  const response = NextResponse.next();
+
+  // const isAuthPage = ['/login', '/auth'].some((path) => request.nextUrl.pathname.startsWith(path));
+
+  // accessToken이 없고 auth 관련 페이지가 아닐때
+  // if (!accessToken && !isAuthPage) {
+  //   return NextResponse.redirect(new URL('/login', request.url));
+  // }
+
+  return response;
+}
