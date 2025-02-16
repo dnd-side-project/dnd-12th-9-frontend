bde0e8fbb56480007712d65099b824b06e4b9b6a
DongjaJ <jdh981118@naver.com>
2025-02-17 01:45:40 +0900

    fix: 불필요한 타입 제거

--- apps/web/app/_api/auth.ts
+++ apps/web/app/_api/auth.ts
@@ -3,7 +3,6 @@ import { instance } from './fetcher';
 export const authRepository = {
   reissue: async (refreshToken: string) =>
     instance.post<{
-      resultType: string;
       data: {
         memberId: string;
       };
