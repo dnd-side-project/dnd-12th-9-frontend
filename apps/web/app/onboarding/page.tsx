49684b8c2bf96c518cf09f31add11c409853aadb
keemsebeen <sebin0580@gmail.com>
2025-02-16 16:09:27 +0900

    refactor: next step 이동 함수 분리

--- apps/web/app/onboarding/page.tsx
+++ apps/web/app/onboarding/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
+
 import { PageLayout } from '@repo/ui/components/Layout';
 import { useFunnel } from 'app/hooks/useFunnel';
 
@@ -9,7 +11,14 @@ import { FUNNEL_STEPS } from './_types/step';
 import { TUTORIAL_CONFIG } from './constants/tutorial';
 
 const OnBoardingPage = () => {
+  const router = useRouter();
   const { Funnel, setStep } = useFunnel(FUNNEL_STEPS);
+
+  const handleStepChange = (steps: string[], index: number) => {
+    const nextStep = steps[index + 1];
+    if (nextStep) return setStep(nextStep);
+    router.push('/home');
+  };
 
   return (
     <PageLayout className="h-dvh">
@@ -19,10 +28,7 @@ const OnBoardingPage = () => {
             <Motion>
               <Tutorial
                 config={TUTORIAL_CONFIG[step as keyof typeof TUTORIAL_CONFIG]}
-                onStepChange={() => {
-                  const nextStep = steps[index + 1];
-                  if (nextStep) setStep(nextStep);
-                }}
+                onStepChange={() => handleStepChange(steps, index)}
               />
             </Motion>
           </Funnel.Step>
