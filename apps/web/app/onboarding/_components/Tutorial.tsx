5caede0873df96dd810e42594ad587433f9184b1
keemsebeen <sebin0580@gmail.com>
2025-02-16 15:03:39 +0900

    refactor: 튜토리얼 기본/배경 이미지 스타일 수정

--- apps/web/app/onboarding/_components/Tutorial.tsx
+++ apps/web/app/onboarding/_components/Tutorial.tsx
@@ -9,18 +9,18 @@ export const Tutorial = ({ onStepChange, config }: StepProps) => {
   const imageUrl = `/onboarding/${config.imageNumber}.png`;
 
   return (
-    <Flex className="h-dvh w-full flex-col items-center justify-between overflow-hidden">
+    <Flex className="relative h-dvh w-full flex-col items-center justify-between overflow-hidden">
       <Image
         fill
         priority
         src={imageUrl}
         alt="튜토리얼 배경 이미지"
-        className="absolute inset-0 z-0 object-cover md:hidden"
+        className="absolute inset-0 z-0 h-full object-cover md:hidden"
         style={{
           filter: 'blur(1px) brightness(0.98)',
         }}
       />
-      <Flex className="relative h-full w-full flex-1">
+      <Flex className="relative w-full flex-1">
         <Image
           fill
           priority
@@ -31,7 +31,7 @@ export const Tutorial = ({ onStepChange, config }: StepProps) => {
         />
       </Flex>
 
-      <Button size="lg" className="absolute bottom-4 md:w-[380px]" onClick={onStepChange}>
+      <Button size="lg" className="absolute bottom-4 w-[360px] md:w-[380px]" onClick={onStepChange}>
         {config.buttonText}
       </Button>
     </Flex>
