15802cc6eae4b7bfec4fc0a495cec2f624d24013
keemsebeen <sebin0580@gmail.com>
2025-02-18 02:08:18 +0900

    feat: topbar와 drawer menu에서 사용할 icon, title, router 상수 정의

---
+++ apps/web/app/home/_constants/topbar.ts
@@ -0,0 +1,50 @@
+export const TOP_BAR = {
+  MISSION: {
+    icon: '/TopBar/MISSION.png',
+    title: '미션하기',
+    router: '/mission',
+  },
+  CLOSET: {
+    icon: '/TopBar/CLOSET.png',
+    title: '꾸미기',
+    router: '/closet',
+  },
+  STORE: {
+    icon: '/TopBar/STORE.png',
+    title: '스토어',
+    router: '/store',
+  },
+  share: {
+    icon: '/TopBar/SHARE.png',
+    title: '자랑하기',
+    router: '/share',
+  },
+} as const;
+
+export const DRAWER_MENU = {
+  HOME: {
+    icon: 'home',
+    title: '홈',
+    router: '/',
+  },
+  SEARCH: {
+    icon: 'search',
+    title: '도서 검색',
+    router: '/search',
+  },
+  STORE: {
+    icon: 'shop',
+    title: '스토어',
+    router: '/store',
+  },
+  CLOSET: {
+    icon: 'bag',
+    title: '꾸미기',
+    router: '/closet',
+  },
+  SHARE: {
+    icon: 'setting',
+    title: '설정',
+    router: '/setting',
+  },
+} as const;
