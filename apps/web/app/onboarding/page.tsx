8cb55c7e8e9b1bf3364b5365efce75412304a65c
keemsebeen <sebin0580@gmail.com>
2025-02-18 02:08:50 +0900

    feat: summary 컴포넌트 구현

--- apps/web/app/onboarding/page.tsx
+++ apps/web/app/onboarding/page.tsx
@@ -3,7 +3,7 @@
 import { useRouter } from 'next/navigation';
 
 import { PageLayout } from '@repo/ui/components/Layout';
-import { useFunnel } from 'app/hooks/useFunnel';
+import { useFunnel } from 'app/_hooks/useFunnel';
 
 import { Motion } from './_components/Motion';
 import { Tutorial } from './_components/Tutorial';
