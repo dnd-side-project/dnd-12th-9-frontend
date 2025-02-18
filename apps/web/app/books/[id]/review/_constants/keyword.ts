0c3197250952c9eb9b8587334926ff5758e5a2c9
DongjaJ <jdh981118@naver.com>
2025-02-18 17:59:07 +0900

    feat: 키워드 관련 ui 추가

---
+++ apps/web/app/books/[id]/review/_constants/keyword.ts
@@ -0,0 +1,78 @@
+export type KeywordType = 'GOOD' | 'SHAME';
+
+export const KEYWORD_MAP = {
+  GOOD: [
+    {
+      id: 101,
+      keyword: 'IMMERSIVE',
+      description: '몰입감 높은',
+    },
+    {
+      id: 102,
+      keyword: 'EMOTIONAL',
+      description: '감동적인',
+    },
+    {
+      id: 103,
+      keyword: 'INFORMATIVE',
+      description: '유익한',
+    },
+    {
+      id: 104,
+      keyword: 'RECOMMENDABLE',
+      description: '추천하고 싶은',
+    },
+    {
+      id: 105,
+      keyword: 'STIMULATE_DOPAMINE',
+      description: '도파민을 자극하는',
+    },
+    {
+      id: 106,
+      keyword: 'BEAUTIFUL_SENTENCE',
+      description: '문장이 아름다운',
+    },
+    {
+      id: 107,
+      keyword: 'KNOWLEDGEABLE',
+      description: '새로운 지식을 주는',
+    },
+    {
+      id: 108,
+      keyword: 'EASY_TO_READ',
+      description: '읽기 부담없는',
+    },
+    {
+      id: 109,
+      keyword: 'LONG_LASTING_AFTERGLOW',
+      description: '여운이 오래가는',
+    },
+  ],
+  SHAME: [
+    {
+      id: 201,
+      keyword: 'BORING',
+      description: '지루한',
+    },
+    {
+      id: 202,
+      keyword: 'DISAPPOINTING',
+      description: '아쉬운',
+    },
+    {
+      id: 203,
+      keyword: 'DIFFICULT_SENTENCE',
+      description: '문장이 난해한',
+    },
+    {
+      id: 204,
+      keyword: 'LACK_OF_COHERENCE',
+      description: '개연성이 부족한',
+    },
+    {
+      id: 205,
+      keyword: 'THREADBARE',
+      description: '진부한',
+    },
+  ],
+};
