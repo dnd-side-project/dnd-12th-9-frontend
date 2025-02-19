2726bc887e5be91e6610e650b7915acaf78aa8cc
DongjaJ <jdh981118@naver.com>
2025-02-19 14:52:59 +0900

    feat: 미션카드 텍스트, 클릭시 핸들러, 아이콘 변경

--- apps/web/app/mission/_components/MissionCardList.tsx
+++ apps/web/app/mission/_components/MissionCardList.tsx
@@ -1,37 +1,50 @@
 'use client';
 
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+
+import BLACKY_DONE from '@public/BLACKY_DONE.png';
+
 import { Button } from '@repo/ui/components/Button';
+import { Icon } from '@repo/ui/components/Icon';
 import { Stack } from '@repo/ui/components/Layout';
 
 import { MissionCard } from './MissionCard';
 
 export const MissionCardList = () => {
+  const router = useRouter();
+
+  const goBooksPage = () => router.push('/books');
+
   return (
     <Stack className="gap-3 px-4">
       <MissionCard
         title="새로운 책 등록하기"
         orbCount={10}
+        missionIcon={<Icon type="missionCard" />}
         action={
-          <Button variant="primary50" size="sm" className="font-medium">
-            책장가기
+          <Button variant="primary50" size="sm" className="font-medium" onClick={goBooksPage}>
+            하러 가기
           </Button>
         }
       />
       <MissionCard
-        title="책 완독하기"
+        title="책 평가하기"
         orbCount={50}
+        missionIcon={<Image src={BLACKY_DONE} width={40} height={40} alt="책 평가" />}
         action={
-          <Button variant="primary50" size="sm" className="font-medium">
-            책장가기
+          <Button variant="primary50" size="sm" className="font-medium" onClick={goBooksPage}>
+            하러 가기
           </Button>
         }
       />
       <MissionCard
         title="독서 카드 공유하기"
         orbCount={50}
+        missionIcon={<Icon type="missionShare" width={50} height={50} />}
         action={
-          <Button variant="primary50" size="sm" className="font-medium">
-            책장가기
+          <Button variant="primary50" size="sm" className="font-medium" disabled>
+            하러 가기
           </Button>
         }
       />
