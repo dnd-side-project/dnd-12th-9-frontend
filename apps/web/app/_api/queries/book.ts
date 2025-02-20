1d895f0f3d77df5da334620a7dce1c3495a1269e
DongjaJ <jdh981118@naver.com>
2025-02-20 12:50:33 +0900

    feat: 책장 조회, 책 상세 조회 api 함수 추가

---
+++ apps/web/app/_api/queries/book.ts
@@ -0,0 +1,41 @@
+import { queryOptions } from '@tanstack/react-query';
+
+import { READING_STATUS } from 'app/_constants/status';
+
+import { fetcher } from '../fetcher';
+import {
+  GetBookDetailAPIRequest,
+  GetBookDetailAPIResponse,
+  GetBookListAPIRequest,
+  GetBookListAPIResponse,
+} from '../types/book';
+
+export const bookQueryKeys = {
+  all: () => ['books'],
+  lists: () => [...bookQueryKeys.all(), 'list'],
+  details: () => [...bookQueryKeys.all(), 'detail'],
+};
+
+export const bookQueryOptions = {
+  list: (memberId: string, readStatus?: READING_STATUS) =>
+    queryOptions({
+      queryKey: [...bookQueryKeys.lists(), memberId, readStatus],
+      queryFn: () => getBookListAPI({ memberId, readStatus }),
+    }),
+  detail: (bookId: string) =>
+    queryOptions({
+      queryKey: [...bookQueryKeys.details(), bookId],
+      queryFn: () => getBookDetailAPI({ bookId }),
+    }),
+};
+
+const getBookListAPI = ({ memberId, readStatus }: GetBookListAPIRequest) => {
+  const searchParams = readStatus ? { readStatus } : undefined;
+
+  fetcher.get<GetBookListAPIResponse>(`books/members/${memberId}`, {
+    searchParams,
+  });
+};
+
+const getBookDetailAPI = ({ bookId }: GetBookDetailAPIRequest) =>
+  fetcher.get<GetBookDetailAPIResponse>(`books/${bookId}`);
--- apps/web/app/_api/types/book.ts
+++ apps/web/app/_api/types/book.ts
@@ -1,3 +1,5 @@
+import { READING_STATUS } from 'app/_constants/status';
+
 export type Book = {
   title: string;
   author: string;
@@ -27,5 +29,45 @@ export type SearchBookResponse = {
     code: string;
     message: string;
     data: object;
+  };
+};
+
+export type MemberBook = Book & {
+  readStatus: READING_STATUS;
+  publishedAt?: '2025-02-20';
+  createdAt?: '2025-02-20T03:32:50.750Z';
+  completedAt?: '2025-02-20';
+};
+
+export type GetBookListAPIRequest = {
+  memberId: string;
+  readStatus?: READING_STATUS;
+};
+
+export type GetBookListAPIResponse = {
+  resultType: 'SUCCESS';
+  data: {
+    bookList: MemberBook[];
+  };
+  error: {
+    code: 'string';
+    message: 'string';
+    data: {};
+  };
+};
+
+export type GetBookDetailAPIRequest = {
+  bookId: string;
+};
+
+export type GetBookDetailAPIResponse = {
+  resultType: 'SUCCESS';
+  data: {
+    bookList: MemberBook;
+  };
+  error: {
+    code: 'string';
+    message: 'string';
+    data: {};
   };
 };
