aaf31507571fa42d9a74e8f305507789f245c17b
keemsebeen <sebin0580@gmail.com>
2025-02-20 20:20:32 +0900

    feat: member point info 컴포넌트 구현

---
+++ apps/web/app/home/_components/MemberPointInfo.tsx
@@ -0,0 +1,76 @@
+'use client';
+import { useRouter } from 'next/navigation';
+
+import { useQuery } from '@tanstack/react-query';
+
+import { Button } from '@repo/ui/components/Button';
+import { OrgIcon } from '@repo/ui/components/Button/assets/OrbIcon';
+import { Icon } from '@repo/ui/components/Icon';
+import { Box, Flex, HStack, JustifyBetween, Spacing } from '@repo/ui/components/Layout';
+import { Text } from '@repo/ui/components/Text';
+import { memberQueryOptions } from 'app/_api/queries/member';
+
+type MemberPointInfoProps = {
+  memberId: string;
+};
+
+export const MemberPointInfo = ({ memberId }: MemberPointInfoProps) => {
+  const router = useRouter();
+
+  // TODO : useSuspenseQueries 변경
+  const { data } = useQuery(memberQueryOptions.point());
+  const { data: bookData } = useQuery(memberQueryOptions.completed(memberId));
+
+  return (
+    <Flex className="flex-col gap-3">
+      <Box className="flex-col gap-3 rounded-lg bg-white px-4 pt-4">
+        <JustifyBetween className="pb-3">
+          <HStack className="items-center gap-1">
+            <Text type="Body2" className="mr-1 text-gray-600">
+              보유 구슬
+            </Text>
+            <OrgIcon />
+            <Text type="Title2" weight="medium" className="text-black">
+              {data?.data.point ?? 0}
+            </Text>
+          </HStack>
+          <Flex className="gap-1">
+            <Text type="Body2" className="text-gray-800">
+              뽑기 가능 횟수
+            </Text>
+            <Text type="Body2" className="text-green-600">
+              {data?.data.drawCount}회
+            </Text>
+          </Flex>
+        </JustifyBetween>
+        <Spacing className="h-[1px] bg-gray-200" />
+        <JustifyBetween className="pb-3 pt-3">
+          <HStack className="items-center gap-2">
+            <Text type="Body1" className="text-gray-600">
+              지금까지 완독한 책
+            </Text>
+            <Text type="Heading1" weight="semibold" className="text-gray-900">
+              {bookData?.data.completedBookCount}권
+            </Text>
+          </HStack>
+          <Button
+            size="sm"
+            variant="primary50"
+            right={<Icon type="next" size={16} color="primary" />}
+            onClick={() => router.push('/books')}
+          >
+            책장가기
+          </Button>
+        </JustifyBetween>
+      </Box>
+      <Button
+        size="lg"
+        variant="primary500"
+        className="w-full"
+        onClick={() => router.push('/search')}
+      >
+        책 추가하기
+      </Button>
+    </Flex>
+  );
+};
