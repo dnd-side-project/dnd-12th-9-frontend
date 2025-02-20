841b6dcc4e3ae4b9551d0c736d50d87c5c65b90e
keemsebeen <sebin0580@gmail.com>
2025-02-21 01:28:11 +0900

    refactor: 홈 페이지 prefetchQuery 추가

--- apps/web/app/home/page.tsx
+++ apps/web/app/home/page.tsx
@@ -3,6 +3,9 @@ import { redirect } from 'next/navigation';
 
 import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
 
+import { bookQueryOptions } from 'app/_api/queries/book';
+import { itemQueryOptions } from 'app/_api/queries/item';
+import { memberQueryOptions } from 'app/_api/queries/member';
 import { COOKIE_ID } from 'app/_constants/cookie';
 
 import { Home } from './_components/Home';
@@ -16,6 +19,12 @@ const HomePage = async () => {
   if (!memberId) {
     redirect('/login');
   }
+
+  await Promise.all([
+    queryClient.prefetchQuery(bookQueryOptions.count(memberId)),
+    queryClient.prefetchQuery(memberQueryOptions.point()),
+    queryClient.prefetchQuery(itemQueryOptions.equipped()),
+  ]);
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
