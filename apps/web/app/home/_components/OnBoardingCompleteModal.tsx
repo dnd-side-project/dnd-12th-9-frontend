bdc66d35badf76d6445204c260d3ce1def847262
keemsebeen <sebin0580@gmail.com>
2025-02-21 09:56:58 +0900

    feat: 온보딩 이후 사용할 모달 추가

---
+++ apps/web/app/home/_components/OnBoardingCompleteModal.tsx
@@ -0,0 +1,51 @@
+import { useRouter } from 'next/navigation';
+
+import { useState } from 'react';
+
+import { Button } from '@repo/ui/components/Button';
+import { CenterStack, JustifyBetween } from '@repo/ui/components/Layout';
+import { Modal } from '@repo/ui/components/Modal';
+import { Text } from '@repo/ui/components/Text';
+
+type OnboardingCompleteModalProps = {
+  isOnBoardingModalOpen: boolean;
+};
+export const OnBoardingCompleteModal = ({
+  isOnBoardingModalOpen,
+}: OnboardingCompleteModalProps) => {
+  const router = useRouter();
+  const [isOpen, setIsOpen] = useState<boolean>(isOnBoardingModalOpen);
+
+  const handleModalClose = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <Modal isOpen={isOpen} onClickOutside={handleModalClose}>
+      <CenterStack className="gap-2">
+        <Text type="Heading1" weight="semibold" className="text-gray-800">
+          튜토리얼 완료!
+        </Text>
+        <Text type="Body1" className="text-center text-gray-600">
+          {`모든 준비가 끝났어요.\n 이제 스부키와 함께 독서를 즐겨보세요.`}
+        </Text>
+
+        <JustifyBetween className="mt-4 gap-2">
+          <Button size="md" variant="gray100" onClick={handleModalClose}>
+            닫기
+          </Button>
+          <Button
+            size="md"
+            variant="black"
+            onClick={() => {
+              handleModalClose();
+              router.push('/search');
+            }}
+          >
+            책 등록하기
+          </Button>
+        </JustifyBetween>
+      </CenterStack>
+    </Modal>
+  );
+};
