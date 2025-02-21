a379bde819d45bdb63ef99a14ec8814d5a8a9d24
DongjaJ <jdh981118@naver.com>
2025-02-22 01:15:22 +0900

    fix: queryClient 생성 로직 변경

--- apps/web/app/_api/ClientQueryProvider.tsx
+++ apps/web/app/_api/ClientQueryProvider.tsx
@@ -1,19 +1,33 @@
 'use client';
-import { useState } from 'react';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: 3,
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
+}
+
+let clientQueryClient: QueryClient | undefined = undefined;
+
+function getQueryClient() {
+  if (typeof window === 'undefined') {
+    // Server: always make a new query client
+    return makeQueryClient();
+  } else {
+    // Browser: make a new query client if we don't already have one
+    if (!clientQueryClient) clientQueryClient = makeQueryClient();
+    return clientQueryClient;
+  }
+}
+
 export const ClientQueryProvider = ({ children }: React.PropsWithChildren) => {
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            retry: 3,
-            refetchOnWindowFocus: false,
-          },
-        },
-      })
-  );
+  const queryClient = getQueryClient();
+
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 };
