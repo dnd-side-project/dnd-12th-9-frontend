97458f88aa7e572a3b6f396ed53659547dc40227
keemsebeen <sebin0580@gmail.com>
2025-02-21 22:06:52 +0900

    feat: book card max width 수정

--- apps/web/app/search/_components/BookCard.tsx
+++ apps/web/app/search/_components/BookCard.tsx
@@ -18,13 +18,13 @@ export const BookCard = ({ title, author, publishedAt, thumbnail, openModal }: B
           height={110}
           className="h-[110px] w-[72px] object-contain"
         />
-        <Flex className="w-full max-w-[220px] flex-col">
+        <Flex className="w-full max-w-40 flex-col md:max-w-[220px]">
           <Text type="Title2" className="text-gray-900">
             {title}
           </Text>
           <Spacing className="h-1" />
           <Flex className="text-gray-600">
-            <Text type="Body2" className="line-clamp-1 max-w-24">
+            <Text type="Body2" className="line-clamp-1 max-w-16 md:max-w-24">
               {author}
             </Text>
             <Text type="Body2">・</Text>
