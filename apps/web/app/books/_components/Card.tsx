7221dcd72373f803535b1a98fffec972c134a95e
keemsebeen <sebin0580@gmail.com>
2025-04-08 01:36:04 +0900

    refactor: Card 컴포넌트 width, height, 기본 이미지 지정

--- apps/web/app/books/_components/Card.tsx
+++ apps/web/app/books/_components/Card.tsx
@@ -10,16 +10,25 @@ type CardProps = {
 };
 
 export const Card = ({ imageURL, title, description }: CardProps) => {
+  const IMAGE = '/Ghost/BASIC_GHOST.webp';
+
   return (
     <Stack className="h-[204px] w-[106px] gap-1">
-      <Box className="border-gray-70 aspect-1 relative h-[156px] rounded-lg border">
-        <Image src={imageURL} alt={title} fill className="border-gray-70 rounded-lg border" />
+      <Box className="border-gray-70 relative rounded-lg border">
+        <Image
+          src={imageURL || IMAGE}
+          width={106}
+          height={156}
+          priority
+          alt={title}
+          className="border-gray-70 h-[156px] rounded-lg border object-cover"
+        />
       </Box>
       <Stack className="gap-0.5">
-        <Text type="Title2" weight="medium" className="text-gray-900">
+        <Text type="Title2" weight="medium" className="line-clamp-2 text-gray-900">
           {title}
         </Text>
-        <Text type="Title2" weight="medium" className="text-gray-400">
+        <Text type="Title2" weight="medium" className="line-clamp-1 text-gray-400">
           {description}
         </Text>
       </Stack>
