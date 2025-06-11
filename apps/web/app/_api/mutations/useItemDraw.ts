6655acf6d0db5fb928626589f6b4bb112c041581
DongjaJ <jdh981118@naver.com>
2025-06-11 21:00:16 +0900

    feat: 아이템 뽑기, 책 등록 시 포인트 리패치하도록 설정

--- apps/web/app/_api/mutations/useItemDraw.ts
+++ apps/web/app/_api/mutations/useItemDraw.ts
@@ -1,12 +1,20 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import { fetcher } from '../fetcher';
+import { bookQueryKeys } from '../queries/book';
 import { ItemDrawResponse } from '../types/draw';
 
 const itemDraw = () => fetcher.post<ItemDrawResponse>('items/draw');
 
 export const useItemDraw = () => {
+  const queryClient = useQueryClient();
+
   return useMutation<ItemDrawResponse, Error>({
     mutationFn: itemDraw,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [...bookQueryKeys.point()],
+      });
+    },
   });
 };
